e84ac307c22dd7f2ef12381e42f04164a1cc506c
Ishaan Das <44090243+ishaanndas@users.noreply.github.com>
2023-10-24 02:05:18 +0530

    Update site.js

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ const obj1 = {
     m: 30,
     h: 2,
     d: 4,
-    title: "Hi",
+    title: "Story1",
     desc: "Friends, and love it what people dont realize is that public enemy already had this cult of skateboarders that loved them. they already had those formerly punk rock white boys loving them, largely because the beastie boys would play that tape for all their friends. it was the record we.",
 };
 const obj2 = {
@@ -15,7 +15,7 @@ const obj2 = {
     m: 1,
     h: 2,
     d: 4,
-    title: "Hi",
+    title: "Story2",
     desc: "Nobody stands in between me and my man its me and mr. jones what kind of fuckery is this? you made me miss the slick rick gig and thought i didnt love you when i did cant believe you played me out like that no, you aint worth guest list.",
 };
 const objs = [obj1, obj2];
