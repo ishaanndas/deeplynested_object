978769ad86a41cd28520a5ef0ab5e4a530e253cc
Ishaan Das <44090243+ishaanndas@users.noreply.github.com>
2023-10-31 02:02:32 +0530

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,24 +1,49 @@
 const obj1 = {
-    name: "Character1",
-    age: 12,
-    gender: "Male",
-    m: 30,
-    h: 2,
-    d: 4,
-    title: "Story1",
-    desc: "Friends, and love it what people dont realize is that public enemy already had this cult of skateboarders that loved them. they already had those formerly punk rock white boys loving them, largely because the beastie boys would play that tape for all their friends. it was the record we.",
-};
-const obj2 = {
-    name: "Character2",
-    age: 12,
+    name: "Wasteland Survivor",
+    age: 28,
     gender: "Female",
-    m: 1,
-    h: 2,
-    d: 4,
-    title: "Story2",
-    desc: "Nobody stands in between me and my man its me and mr. jones what kind of fuckery is this? you made me miss the slick rick gig and thought i didnt love you when i did cant believe you played me out like that no, you aint worth guest list.",
+    m: 20,
+    h: 3,
+    d: 7,
+    title: "The Last Hope",
+    desc: "In a world ravaged by nuclear fallout, Wasteland Survivor is a beacon of hope. Her mental stability, physical health, and unwavering defense skills are unmatched. She roams the desolate landscape, leading those who dare to dream of a better future."
 };
-const objs = [obj1, obj2];
+
+const obj2 = {
+    name: "The Lone Nomad",
+    age: 32,
+    gender: "Non-binary",
+    m: 15,
+    h: 1,
+    d: 5,
+    title: "Echoes of a Broken World",
+    desc: "In the desolation of a post-apocalyptic world, The Lone Nomad wanders alone, haunted by memories of the past. Their mental stability is tested daily, health is frail, and their defense mechanisms are a mere shield against the harsh reality. The story reveals their journey to find meaning amid the ruins."
+};
+
+const obj3 = {
+    name: "Cyberpunk Survivor",
+    age: 25,
+    gender: "Male",
+    m: 40,
+    h: 4,
+    d: 8,
+    title: "Rise of the Machines",
+    desc: "In a dystopian future where machines rule, Cyberpunk Survivor is the ultimate resistance. His unshakable mental stability, unparalleled health, and impenetrable defense make him a legend. The story unfolds as he battles the mechanical overlords to reclaim a shattered world."
+};
+
+const obj4 = {
+    name: "The Forgotten Hero",
+    age: 35,
+    gender: "Female",
+    m: 10,
+    h: 2,
+    d: 3,
+    title: "A World Unraveled",
+    desc: "Once celebrated, now forgotten - The Forgotten Hero navigates a world torn apart by cataclysm. Her mental stability wanes, health deteriorates, and defense mechanisms fail. The story explores her quest for survival and redemption in a world that's lost all hope."
+};
+
+
+const objs = [obj1, obj2, obj3, obj4];
 const randomObj = objs[Math.floor(Math.random() * objs.length)];
 document.getElementById("name").textContent = randomObj.name;
 document.getElementById("age").textContent = randomObj.age.toString();
